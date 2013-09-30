4b931d8029e52bf92c1c08c6fbf9981977d8e688
Winston Hearn <winston@wnstn.com>
2013-09-30 14:11:45 -0700

    adding my customizations

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -36,9 +36,7 @@ alias afind='ack -il'
 
 alias astart="sudo apachectl start"
 alias astop="sudo apachectl stop"
-alias arestart="sudo apachectl restart"
-alias mysqlstart="sudo /usr/local/mysql/support-files/mysql.server start"
-alias mysqlstart="sudo /usr/local/mysql/support-files/mysql.server stop"
+alias arestart="sudo apachectl stop"
 alias hosts="sudo st /etc/hosts"
 alias conf="sudo st /etc/apache2/httpd.conf"
 
@@ -48,21 +46,8 @@ alias gcom="git commit -m"
 alias glog="git log --graph --pretty=format':%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset'"
 alias gprm="git pull --rebase master"
 alias gpom="git pull origin master"
-alias gcoe="git commit -e"
 
 # Rails
 alias migrate="rake db:migrate && rake db:test:prepare"
 alias be="bundle exec"
 alias rrg="rake routes | grep"
-
-# Custom SSH stuff
-alias codeblog='ssh -t root@107.170.73.239 "cd ../var/www/ghost/ ; bash"'
-alias fcssh='ssh winston@app1-tx.prod.foxycart.com'
-
-# Projects
-alias st53='cd ~/Sites/ST && st fiftythree.sublime-project && cd -'
-alias stfoxy='cd ~/Sites/ST && st foxycart.sublime-project && cd -'
-alias stiht='cd ~/Sites/ST && st iht.sublime-project && cd -'
-alias stng='cd ~/Sites/ST && st numbergarage.sublime-project && cd -'
-alias stplay='cd ~/Sites/ST && st play.sublime-project && cd -'
-alias stss='cd ~/Sites/ST && st split-secnd.sublime-project && cd -'
